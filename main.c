46c8464e1a2df20292c6ac7f2e57476f147c7394
Punyashree966 <sripunyaa966@gmail.com>
2026-06-09 23:34:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,46 @@
 #include<stdio.h>
-#define HEIGHT 20
-#define WIDTH 40
-char canvas[HEIGHT][WIDTH];
+#include<math.h>
+#define ROWS 10
+#define COLS 20
+char canvas[ROWS][COLS];
 void intializeCanvas()
 {
-  int i,j;
-for(i=0;i<HEIGHT;i++)
+for(int i=0;i<ROWS;i++)
 {
-for(j=0;j<WIDTH;j++)
+for(int j=0;j<COLS;j++)
 {
 canvas[i][j]='_';
 }
 }
 }
-void drawLine(int row,int startColumn, int end column)
+void display()
 {
-  int j;
-for(j=startcolumn;j<=endcolumn;j++)
+for(int i=0;i<ROWS;i++)
 {
-  canvas[row][j]='*;
+for(int j=0;j<COLS;j++)
+{
+printf("%c",canvas[i][j]);
+}
+printf("\n");
+}
+}
+void drawRectangle(int row,int col,int height,int width, char ch)
+{
+for(int i=col;i<col+width;i++)
+{
+canvas[row][i]=ch;
+canvas[i][col+width-1]=ch;
+}
+}
+void drawLine(int row,int startCol, int end column,char ch)
+{
+  int i;
+  if(row<0||row>=ROWS)
+    return;
+for(i=startCol;i<=endCol&&i<COLS;i++)
+{
+  if(i>=0)
+  canvas[row][i]=ch;
     }
 }
 void displayCanvas()
