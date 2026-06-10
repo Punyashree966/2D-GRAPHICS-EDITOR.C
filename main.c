f61e4a8108933fe873959af6741d9556e5059357
Punyashree966 <sripunyaa966@gmail.com>
2026-06-10 19:02:22 +0530

    Update main.c

--- main.c
+++ main.c
@@ -35,6 +35,7 @@ canvas[row+height-1][i]=ch;
     {
       canvas[i][col]=ch;
       canvas[i][col+width-1]=ch;
+    }
 }
 void drawLine(int row,int startCol, int end column,char ch)
 {
@@ -47,24 +48,156 @@ for(i=startCol;i<=endCol&&i<COLS;i++)
   canvas[row][i]=ch;
     }
 }
-void displayCanvas()
+void drawTriangle(int row,int col,int height,char ch)
 {
-  int i,j;
-for(i=0;i<HEIGHT;i++)
+for(inti=0;i<height;i++)
 {
-for(j=0;j<WIDTH;j++)
-{
-printf("%c",canvas[i][j]);
+if(row+i<ROWS&&col-i>=0)
+  canvas[row+i][col-i]=ch;
+if(row+i<ROWS&&col+i<COLS)
+  canvas[row+i][col+i]=ch;
 }
-print("\n");
+for(int i=col-height+1;i<=col+height-1;i++)
+{
+if(row+height-1<ROWS&&i>=0&&i<COLS)
+  canvas[row+height-1][i]=ch;
+}
+}
+void drawCircle(int xc,int yc,int radius,char ch)
+{
+int angle;
+for(angle=0;angle<360;angle++)
+{
+int x = xc + radius*cos(angle*3.14159/180);
+int y = yc + radius*sin(angle*3.14159/180);
+if(x>=0 && x<ROWS && y>=0 && y<COLS)
+{
+canvas[x][y]=ch;
+}
 }
 }
 int main()
 {
-  printf("====2D GRAPHICS EDITOR====\n");
+int choice;
 initializeCanvas();
-drawLine(5,10,30);
-displayCanvas();
+do
+{
+printf("\n====2D GRAPHICS EDITOR====\n");
+printf("1.Draw Rectangle\n");
+printf("2.Draw Line\n");
+printf("3.Draw Triangle\n");
+printf("4.Draw Circle\n");
+printf("5.Delete Rectangle\n");
+printf("6.Delete Line\n");
+printf("7.Delete Triangle\n");
+printf("8.Delete Circle\n");
+printf("9.Modify Rectangle\n");
+printf("10.Display Canvas\n");
+printf("11.Clear Canvas\n");
+printf("12.Exit\n");
+printf("Enter Choice:");
+scanf("%d",&choice);
+switch(choice)
+{
+case 1:
+{
+int r,c,h,w;
+printf("Row Col Height Width:");
+scanf("%d%d%d%d",&r,&c,&h,&w);
+drawRectangle(r,c,h,w'*');
+  break;
+}
+  case 2:
+{
+int row,start,end;
+printf("Row StartCol EndCol:");
+  scanf("%d%d%d",&row,&start,&end);
+drawLine(row,start,end,'*');
+break;
+}
+case 3:
+{
+int row,col,height;
+printf("TopRow CentralCol Height:");
+scanf("%d%d%d",&row,&col,&height);
+drawTriangle(row,col,height,'*');
+break;
+}
+case 4:
+{
+int x,y,r;
+printf("CenterRow CenterCol Radius:");
+scanf("%d%d%d",&x,&y,&r);
+drawCircle(x,y,r,'*');
+break;
+}
+case 5:
+{
+int r,c,h,w;
+printf("Row Col Height Width:");
+scanf("%d%d%d%d",&r,&c,&h,&w);
+drawRectangle(r,c,h,w,'_');
+break;
+}
+case 6:
+{
+int row,start,end;
+printf("Row StartCol EndCol:");
+scanf("%d%d%d",&row,&start,&end);
+drawLine(row,start,end,'_');
+break;
+}
+case 7:
+{
+int row,col,height;
+printf("TopRow CenterCol Height:");
+scanf("%d%d%d",&row,&col,&height);
+drawTriangle(row,col,height,'_');
+break;
+}
+case 8:
+{
+int x,y,r;
+printf("CenterRow CenterCol Radius:");
+scanf("%d%d%d",&x,&y,&r);
+drawCircle(x,y,r,'_');
+break;
+}
+case 9:
+  {
+  int r,c,h,w;
+  printf("Old Rectangle Row Col Height Width:");
+  scanf("%d%d%d%d",&r,&c,&h,&w);
+  drawRectangle(r,c,h,w,'_');
+  printf("New Rectangle Row Col Height Width:");
+  scanf("%d%d%d%d",&r,&c,&h,&w);
+  drawRectangle(r,c,h,w,'*');
+  break;
+}
+case 10:
+{
+display();
+break;
+}
+case 11:
+  {
+  initializeCanvas();
+  printf("Canvas Cleared!\n");
+  break;
+}
+  case 12:
+    {
+  printf("Exiting..\n");
+  break;
+  default:
+    printf("Invaild Choice!\n");
+}
+}
+while(choice!=12)
 return 0;
 }
+  }
+
+
+
 
